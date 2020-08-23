16915cafb87ce5a444d9a2494b7f12f1cef6e822
Aitugan <mirash2001@gmail.com>
2020-08-23 23:12:18 +0500

    Added passport to manage authentication with facebook

---
+++ passport.js
@@ -0,0 +1,81 @@
+var passport = require("passport");
+var localStrategy = require("passport-local").Strategy;
+var FacebookStrategy = require("passport-facebook").Strategy;
+
+passport.serializeUser(function (user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findOne({ _id: id }, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(
+  new localStrategy(
+    {
+      usernameField: "email",
+    },
+    function (username, password, done) {
+      User.findOne({ email: username }, function (err, user) {
+        if (err) return done(err);
+        if (!user) {
+          return done(null, false, {
+            message: "Incorrect username or password",
+          });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, {
+            message: "Incorrect username or password",
+          });
+        }
+        return done(null, user);
+      });
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: "612822675590167",
+      clientSecret: "dcb63fc3264a53bc54496cad1ab51da6",
+      callbackURL: "http://localhost:3000/auth/facebook/callback",
+      profileFields: ["id", "displayName", "email"],
+    },
+    function (token, refreshToken, profile, done) {
+      User.findOne({ facebookId: profile.id }, function (err, user) {
+        if (err) return done(err);
+
+        if (user) {
+          return done(null, user);
+        } else {
+          User.findOne({ email: profile.emails[0].value }, function (
+            err,
+            user
+          ) {
+            if (user) {
+              user.facebookId = profile.id;
+              return user.save(function (err) {
+                if (err)
+                  return done(null, false, { message: "Can't save user info" });
+                return done(null, user);
+              });
+            }
+
+            var user = new User();
+            user.name = profile.displayName;
+            user.email = profile.emails[0].value;
+            user.facebookId = profile.id;
+            user.save(function (err) {
+              if (err)
+                return done(null, false, { message: "Can't save user info" });
+              return done(null, user);
+            });
+          });
+        }
+      });
+    }
+  )
+);
