0c8a9a588f81c603ac36ea3c2d945dcd59929134
Aitugan <mirash2001@gmail.com>
2020-08-23 23:11:43 +0500

    Added mailer and db configs to export

---
+++ config.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = {
+  mailer: {
+    service: "Gmail",
+    auth: {
+      user: "mirash2001@gmail.com",
+      pass: "EducationIsGood",
+    },
+  },
+  dbConnstring:
+    "mongodb+srv://admin:EducationProgramming7!@cluster0.ogclc.mongodb.net/CEditorDB?retryWrites=true&w=majority",
+  sessionKey: "Hello everybody!",
+};
